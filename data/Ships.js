e7de8a87fe303f8c6ae7d7cee6fc59fe705feac7
Adam Kovacs <adam.kovacs@Nology.local>
2019-12-02 16:57:20 +0000

    design update

--- data/Ships.js
+++ data/Ships.js
@@ -1,34 +1,32 @@
-export default class MotherShip{
-  constructor(name, HP, HPLossOnHit){
+export default class MotherShip {
+  constructor(name, HP, HPLossOnHit, path) {
     this.name = name;
     this.HP = HP;
     this.HPLossOnHit = HPLossOnHit;
     this.isAlive = true;
+    this.path = path;
   }
 
-  hit(){
-    if(this.HP > this.HPLossOnHit){
-       return this.HP -= this.HPLossOnHit;
-    } 
-    return this.name = "dead", this.HP = 0, this.isAlive = false;
+  hit() {
+    if (this.HP > this.HPLossOnHit) {
+      return (this.HP -= this.HPLossOnHit);
+    }
+    return (this.name = "dead"), (this.HP = 0), (this.isAlive = false);
   }
 
-  render(){
-    return `<div><p id ="${this.name}">${this.name}</p><p>${this.HP}</p></div>`
+  render() {
+    return `<div id="bugContainer"><img src="${this.path}" /><p id ="${this.name}">${this.name}</p><p>HP: ${this.HP}</p></div>`;
   }
-
-} 
-
-export class DefenceShip extends MotherShip{
-  constructor(name, HP, HPLossOnHit){
-    super(name, HP, HPLossOnHit)
-  }
- 
 }
 
-export class AttackShip extends MotherShip{
-  constructor(name, HP, HPLossOnHit){
-    super(name, HP, HPLossOnHit)
+export class DefenceShip extends MotherShip {
+  constructor(name, HP, HPLossOnHit, path) {
+    super(name, HP, HPLossOnHit, path);
   }
+}
 
+export class AttackShip extends MotherShip {
+  constructor(name, HP, HPLossOnHit, path) {
+    super(name, HP, HPLossOnHit, path);
+  }
 }
--- main.js
+++ main.js
@@ -1,58 +1,55 @@
-import  MotherShip, {DefenceShip, AttackShip} from "./data/Ships.js";
-
+import MotherShip, { DefenceShip, AttackShip } from "./data/Ships.js";
 
 let allShips = [];
 
 const createShips = (motherNum, defenceNum, attackNum) => {
-
   for (let index = 0; index < motherNum; index++) {
-    allShips.push(new MotherShip("MotherShip", 100, 9));
+    allShips.push(new MotherShip("Mother of All", 100, 9, "./brainbug.jpg"));
   }
 
   for (let index = 0; index < defenceNum; index++) {
-    allShips.push(new DefenceShip("DefenceShip", 80, 10));
+    allShips.push(new DefenceShip("Defence bug", 80, 10, "./mother.png"));
   }
 
   for (let index = 0; index < attackNum; index++) {
-    allShips.push(new AttackShip("Attackship", 45, 12));
+    allShips.push(
+      new AttackShip("Warrior bug", 45, 12, "./Army of Klendathu.gif")
+    );
   }
-}
+};
 
 createShips(1, 5, 8);
 
-const randomize = (allShips) => {
+const randomize = allShips => {
   let rand = Math.random();
   let totalShips = allShips.length;
   let randIndex = Math.floor(rand * totalShips);
   let randomShip = allShips[randIndex];
   return randomShip;
-}
+};
 
-const renderShips= () => {
+const renderShips = () => {
   let alienShips = document.getElementById("ships");
   alienShips.innerHTML = "";
-  allShips.forEach(i => alienShips.innerHTML += i.render());
-}
+  allShips.forEach(i => (alienShips.innerHTML += i.render()));
+};
 
 const destroy = () => {
-  
   randomize(allShips).hit();
   console.log(randomize(allShips).hit());
   renderShips();
 
   for (let index = 0; index < allShips.length; index++) {
-    if ( allShips[0].HP === 0){
+    if (allShips[0].HP === 0) {
       return alert("You have saved mother earth comrad! Well done!");
-     
     }
-    if (allShips[index].HP <= 0){
+    if (allShips[index].HP <= 0) {
       allShips.splice(index, 1);
-    } 
+    }
   }
-}
+};
 
 let burn = document.getElementById("burn");
 burn.addEventListener("click", destroy);
-
 
 renderShips();
